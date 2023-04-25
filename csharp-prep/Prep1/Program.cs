c14463e568ccb8d1a033dfecfb99db06cd30da31
Use your GitHub username <Enter your email address>
2023-04-25 11:50:15 -0300

    prep 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Whats is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Whats is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine ($"your name is {last_name}, {name} {last_name}.");
     }
 }
