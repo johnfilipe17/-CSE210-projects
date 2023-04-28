5fe2a23fc71446e18658fa4f7768566c9d693cd5
Use your GitHub username <Enter your email address>
2023-04-28 10:29:33 -0300

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*parts 1 and 2 where the user chooses number 
+        Console.Write("What is the magic number?");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("Take a guess:");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (guess > number)
+        {
+            Console.Write("Lower!");
+        }
+
+        else if (guess < number) 
+        {
+            Console.Write("Higher!");
+        }
+
+        else 
+        {
+            Console.Write("You guessed it right!");
+        }*/
+
+        //part 3 where we use a random number 
+        Console.Write("Press any key to play");
+        int guess = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+
+        while (guess != number)
+        {
+            Console.Write("what is your guess?");
+            guess = int.Parse(Console.ReadLine()); 
+
+            if (guess > number)
+            {
+            Console.Write("Lower!");
+            }
+
+            else if (guess < number) 
+            {
+            Console.Write("Higher!");
+            }
+
+            else 
+            {
+            Console.Write("You guessed it right!");
+            }
+        }
+
     }
+
+    
 }
+
